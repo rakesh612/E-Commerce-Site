6b554c2da581549baee7b3ed6d860725dcc8e4b3
Nukala Hanumath Rakesh <rakeshcoc1to11@example.com>
2025-10-13 15:44:28 +0530

    completed

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -3,4 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
+  tls: {}, // important for SSL
+});
