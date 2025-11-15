ab562a2e2264873cde439d732ffeef75f0f06c84
Nukala Hanumath Rakesh <rakeshcoc1to11@example.com>
2025-11-15 11:03:36 +0530

    completed

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,9 +1,5 @@
-
 import { Redis } from '@upstash/redis'
 export const redis = new Redis({
   url: 'https://premium-pug-5942.upstash.io',
   token: 'ARc2AAImcDJhMGUzODQ3YjJiZTM0Nzk2OTJkMWYwY2YwYzQxMGU0N3AyNTk0Mg',
 })
-
-
-
